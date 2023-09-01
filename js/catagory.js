a510696cdefb56991fb81da50a7b6a1b2e4de147
Radiat09 <rzsianboyridoy10@gmail.com>
2023-09-01 17:46:09 +0600

    finished most of the work

--- js/catagory.js
+++ js/catagory.js
@@ -1,9 +1,78 @@
-// const handleCatagory = (catagoryId) => {
-//   console.log(catagoryId);
-// };
-const handleCatagoryEvent = (catagoryId) => {
+const loadSpecificCatagory = async (catagoryId) => {
+  // console.log(catagoryId);
+  const tabActive = document.getElementById(catagoryId);
+  const tabActive1000 = document.getElementById("1000");
+  const tabActive1001 = document.getElementById("1001");
+  const tabActive1003 = document.getElementById("1003");
+  const tabActive1005 = document.getElementById("1005");
+  if (catagoryId === "1000") {
+    tabActive.classList.add("tab-active");
+    tabActive1001.classList.remove("tab-active");
+    tabActive1003.classList.remove("tab-active");
+    tabActive1005.classList.remove("tab-active");
+  } else if (catagoryId === "1001") {
+    tabActive.classList.add("tab-active");
+    tabActive1000.classList.remove("tab-active");
+    tabActive1003.classList.remove("tab-active");
+    tabActive1005.classList.remove("tab-active");
+  } else if (catagoryId === "1003") {
+    tabActive.classList.add("tab-active");
+    tabActive1000.classList.remove("tab-active");
+    tabActive1001.classList.remove("tab-active");
+    tabActive1005.classList.remove("tab-active");
+  } else {
+    tabActive.classList.add("tab-active");
+    tabActive1000.classList.remove("tab-active");
+    tabActive1001.classList.remove("tab-active");
+    tabActive1003.classList.remove("tab-active");
+  }
+  loadingSpinner(true);
   const CardContainer = document.getElementById("card-container");
-
   // When click cardContainer will be empty for clicked catagory
-  // CardContainer.innerHTML = "";
+  CardContainer.innerHTML = "";
+  // console.log(catagoryId);
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${catagoryId}`
+  );
+  const data = await res.json();
+  const dataArry = data.data;
+  // console.log(dataArry);
+  const emptyArr = [];
+  if (dataArry.length === emptyArr.length) {
+    loadingSpinner(false);
+    console.log("No Data");
+    const noDataFoundDiv = document.createElement("div");
+    noDataFoundDiv.classList = `flex flex-col justify-center items-center`;
+    noDataFoundDiv.innerHTML = `
+     <img class="w-[140px] mb-4" src="./images/Icon.png" alt="" />
+     <h3 class="text-3xl font-bold">Oops!! Sorry, There is no content here</h3>
+  
+    `;
+    CardContainer.classList = "";
+    CardContainer.appendChild(noDataFoundDiv);
+  } else {
+    CardContainer.classList = `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center`;
+    handleCatagoryEvent(dataArry);
+  }
+};
+
+const handleCatagoryEvent = (dataArry) => {
+  // console.log(dataArry);
+
+  dataArry.forEach((element) => {
+    // console.log(element);
+    cardElement(element);
+  });
+  loadingSpinner(false);
+};
+
+//Loading Spinner
+const loadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  // console.log(loadingSpinner);
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
 };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+// Utility Function
+const cardElement = (element) => {
+  const CardContainer = document.getElementById("card-container");
+  const card = document.createElement("div");
+  card.classList = `2xl:w-[314px] md:w-[300px] `;
+  let verifiedIcon = "";
+  if (element?.authors[0]?.verified) {
+    verifiedIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+  <g clip-path="url(#clip0_13_1000)">
+    <path d="M19.375 10.0001C19.375 10.8001 18.3922 11.4595 18.1953 12.197C17.9922 12.9595 18.5063 14.022 18.1203 14.6892C17.7281 15.3673 16.5484 15.4486 15.9984 15.9986C15.4484 16.5486 15.3672 17.7282 14.6891 18.1204C14.0219 18.5064 12.9594 17.9923 12.1969 18.1954C11.4594 18.3923 10.8 19.3751 10 19.3751C9.2 19.3751 8.54062 18.3923 7.80312 18.1954C7.04062 17.9923 5.97813 18.5064 5.31094 18.1204C4.63281 17.7282 4.55156 16.5486 4.00156 15.9986C3.45156 15.4486 2.27187 15.3673 1.87969 14.6892C1.49375 14.022 2.00781 12.9595 1.80469 12.197C1.60781 11.4595 0.625 10.8001 0.625 10.0001C0.625 9.20012 1.60781 8.54075 1.80469 7.80325C2.00781 7.04075 1.49375 5.97825 1.87969 5.31106C2.27187 4.63293 3.45156 4.55168 4.00156 4.00168C4.55156 3.45168 4.63281 2.272 5.31094 1.87981C5.97813 1.49387 7.04062 2.00793 7.80312 1.80481C8.54062 1.60793 9.2 0.625122 10 0.625122C10.8 0.625122 11.4594 1.60793 12.1969 1.80481C12.9594 2.00793 14.0219 1.49387 14.6891 1.87981C15.3672 2.272 15.4484 3.45168 15.9984 4.00168C16.5484 4.55168 17.7281 4.63293 18.1203 5.31106C18.5063 5.97825 17.9922 7.04075 18.1953 7.80325C18.3922 8.54075 19.375 9.20012 19.375 10.0001Z" fill="#2568EF"/>
+    <path d="M12.7093 7.20637L9.14053 10.7751L7.29053 8.92668C6.88897 8.52512 6.2374 8.52512 5.83584 8.92668C5.43428 9.32824 5.43428 9.97981 5.83584 10.3814L8.43115 12.9767C8.82178 13.3673 9.45615 13.3673 9.84678 12.9767L14.1624 8.66106C14.564 8.25949 14.564 7.60793 14.1624 7.20637C13.7608 6.80481 13.1108 6.80481 12.7093 7.20637Z" fill="#FFFCEE"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_13_1000">
+      <rect width="20" height="20" fill="white"/>
+    </clipPath>
+  </defs>
+</svg>`;
+  } else {
+    verifiedIcon = "";
+  }
+  card.innerHTML = `
+    <img src="${element?.thumbnail}" alt="" class="w-[312px] h-[200px] rounded-lg" />
+    <div class="flex gap-4 mt-4">
+    <img
+    src="${element?.authors[0]?.profile_picture}"
+    alt=""
+    class="w-[40px] h-[40px] rounded-full"
+    />
+    <div class="flex flex-col justify-start items-start">
+    <h4 class="text-primary font-bold">
+    ${element?.title}
+    </h4>
+      <div class="flex justify-center items-center gap-2">
+        <p class="text-second my-2">${element?.authors[0]?.profile_name}</p><span>${verifiedIcon}</span>
+        </div>
+        <p class="text-second">${element?.others?.views} Views</p>
+    </div>
+    </div>
+    `;
+  CardContainer.appendChild(card);
+};
+
 // Handle catagory
 const handleCatagory = async () => {
   const res = await fetch(
@@ -6,14 +49,14 @@ const handleCatagory = async () => {
   const data = await res.json();
 
   const catagoryData = data.data;
-  console.log(catagoryData);
+  // console.log(catagoryData);
   const tabContainer = document.getElementById("tab-container");
   catagoryData.forEach((element) => {
-    console.log(element?.category_id);
+    // console.log(element?.category_id);
     const tabDiv = document.createElement("div");
     tabDiv.classList.add("tabs", "tabs-boxed", "font-bold");
     tabDiv.innerHTML = `
-    <a onclick="handleCatagoryEvent('${element?.category_id}')" class="tab ">${element?.category}</a>
+    <a id="${element?.category_id}" onclick="loadSpecificCatagory('${element?.category_id}'),loadingSpinner('true')" class="tab">${element?.category}</a>
     `;
     tabContainer.appendChild(tabDiv);
   });
@@ -31,32 +74,15 @@ const loadCatagory = async () => {
   // displayPhone(phones, isShowMore);
 };
 
+// display All catagory
 const displayAllCatagory = (allCatagory) => {
   // console.log(allCatagory);
-  const CardContainer = document.getElementById("card-container");
+
   allCatagory.forEach((element) => {
-    console.log(element);
-    const card = document.createElement("div");
-    card.classList = "w-[314px]";
-    card.innerHTML = `
-    <img src="${element?.thumbnail}" alt="" class="w-[312px] h-[200px] rounded-lg" />
-    <div class="flex gap-4 mt-4">
-    <img
-    src="${element?.authors[0]?.profile_picture}"
-    alt=""
-    class="w-[40px] h-[40px] rounded-full"
-    />
-    <div>
-    <h4 class="text-primary font-bold">
-    Building a Winning UX Strategy Using the Kano Model
-    </h4>
-    <p class="text-second my-2">${element?.authors[0]?.profile_name} <span>${element?.authors[0]?.verified}</span></p>
-    <p class="text-second">${element?.others?.views} Views</p>
-    </div>
-    </div>
-    `;
-    CardContainer.appendChild(card);
+    // console.log(element);
+    cardElement(element);
   });
+  loadingSpinner(false);
 };
 handleCatagory();
 loadCatagory();
